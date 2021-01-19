2efb78f95bf8752284d48d17ca1aefb0eec64ccd
rfarofias <rfarofias@gmail.com>
2021-01-19 23:15:00 +0000

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/rfarofias/cursomc/domain/Cliente.java
+++ src/main/java/com/rfarofias/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.rfarofias.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -28,6 +29,7 @@ public class Cliente implements Serializable {
 	private String nif;
 	private Integer tipo;
 
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Morada> moradas = new ArrayList<>();
 	
--- src/main/java/com/rfarofias/cursomc/domain/Morada.java
+++ src/main/java/com/rfarofias/cursomc/domain/Morada.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Morada implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Morada implements Serializable {
 	private String bairro;
 	private String cep;
 
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -30,6 +33,10 @@ public class Morada implements Serializable {
 	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
 
+	public Morada() {
+		
+	}
+	
 	public Morada(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
 			Cliente cliente, Cidade cidade) {
 		super();
