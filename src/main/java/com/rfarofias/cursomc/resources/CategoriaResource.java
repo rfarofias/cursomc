9c531d3ece19530f78321e30ae8649718592cffe
rfarofias <rfarofias@gmail.com>
2021-01-19 20:08:23 +0000

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/rfarofias/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rfarofias/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.rfarofias.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +20,8 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.find(id);		
-		return ResponseEntity.ok(obj);
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 }
