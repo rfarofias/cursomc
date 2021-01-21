aca5340294031df9010ca79f3365ec5b50d9479d
rfarofias <rfarofias@gmail.com>
2021-01-21 19:59:20 +0000

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/rfarofias/cursomc/domain/Cliente.java
+++ src/main/java/com/rfarofias/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.rfarofias.cursomc.domain.enums.TipoCliente;
 
@@ -38,6 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany
 	@JoinColumn(name = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
--- src/main/java/com/rfarofias/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/rfarofias/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rfarofias.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern="yyy-mm-dd")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="yyy-mm-dd")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
